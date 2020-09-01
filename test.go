7b88628f5c0d1a87a13714a7c12b277f06366606
unknown <jjomacson@gmail.com>
2020-09-01 21:43:51 +0900

    ok

---
+++ test.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s1 string = "hello world go!!"
+	var s2 string = "한글"
+	var V1 bool = true
+	var V2 bool = false
+
+	fmt.Println("s1 = ", len(s1))
+	fmt.Println("s2 = ", len(s2))
+	fmt.Println(s1 == s2)
+
+	fmt.Println(V1)
+	fmt.Println(V2)
+}
